3eecfa20e060b43ab8b990877916910267efe95a
Guilherme Kammsetzer <guilherme@kammsetzer.dev>
2022-09-28 16:22:51 -0400

    feat: improve websockets server and add wip d3 frontend
    Add more capabilites to the websocket server and to the simulation.
    The simulation can now have a different number of philosophers.
    The simulation now can be started and stopped with websocket messages.
    Add wip d3 frontend page for the simulation. (still needs quite a bit of
    work, currently there are only some static shapes drawn on the screen.
    The simulation is not running on cli anymore, we could create a new
    cli.js that will run it on the command line if needed.

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 import { Worker } from 'node:worker_threads'
-const numberOfPhilosophers = 5
 
-const buffer = new SharedArrayBuffer(
-  numberOfPhilosophers * Int32Array.BYTES_PER_ELEMENT
-)
+// TODO: Should we even have a default here?
+const defaultNumberOfPhilosophers = 5
 
-const forks = new Int32Array(buffer)
+export const createSimulation = () => {
+  const state = {
+    forks: undefined,
+    philosophers: undefined,
+    running: false
+  }
 
-export const philosophers = Array(numberOfPhilosophers)
-  .fill(null)
-  .map(
-    (_, philosopher, { length }) =>
-      new Worker('./philosopher.js', {
-        workerData: {
-          philosopher,
-          // resource hierarchy logic
-          fork1: philosopher < length - 1 ? philosopher : 0,
-          fork2: philosopher < length - 1 ? philosopher + 1 : philosopher
-        }
-      })
-  )
+  const start = (numberOfPhilosophers = defaultNumberOfPhilosophers) => {
+    const buffer = new SharedArrayBuffer(
+      numberOfPhilosophers * Int32Array.BYTES_PER_ELEMENT
+    )
 
-philosophers.forEach(p => p.postMessage(forks))
+    state.forks = new Int32Array(buffer)
+
+    state.philosophers = Array(numberOfPhilosophers)
+      .fill(null)
+      .map(
+        (_, philosopher, { length }) =>
+          new Worker('./philosopher.js', {
+            workerData: {
+              philosopher,
+              // resource hierarchy logic
+              fork1: philosopher < length - 1 ? philosopher : 0,
+              fork2: philosopher < length - 1 ? philosopher + 1 : philosopher
+            }
+          })
+      )
+
+    state.philosophers.forEach(p => p.postMessage(state.forks))
+    state.running = true
+  }
+
+  const stop = () => {
+    state.philosophers.forEach(p => p.terminate())
+    state.forks = undefined
+    state.philosophers = undefined
+    state.running = false
+  }
+
+  return { stop, start, state }
+}
--- webserver.js
+++ webserver.js
@@ -1,24 +1,67 @@
 import Fastify from 'fastify'
 import fastifyWs from '@fastify/websocket'
+import fastifyStatic from '@fastify/static'
+import path from 'path'
+import url from 'url'
+
 const fastify = Fastify()
 
-import { philosophers } from './index.js'
+import { createSimulation } from './index.js'
 
-fastify.register(fastifyWs)
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+
+fastify
+  .register(fastifyWs)
+  // TODO: fix / not redirecting to /index.html
+  .register(fastifyStatic, { root: path.join(__dirname, 'public') })
+
+const Msg = {
+  ALREADY_RUNNING: 'sim::alreadyRunning',
+  ALREADY_STOPPED: 'sim::alreadyStopped',
+  STOPPED: 'sim::stopped',
+  STARTED: 'sim::started',
+  UPDATED: 'sim::updated'
+}
+
+const { start, stop, state } = createSimulation()
 
 fastify.register(async function (fastify) {
   fastify.get('/', { websocket: true }, connection => {
-    connection.socket.on('')
-    philosophers.forEach(p =>
-      p.on('message', workerMessage => {
-        connection.socket.send(JSON.stringify(workerMessage))
-      })
-    )
+    const sendMessage = (eventType, payload) => {
+      connection.socket.send(JSON.stringify({ type: eventType, payload }))
+    }
+
+    connection.socket.on('message', msg => {
+      const message = msg.toString()
+      if (message === 'start') {
+        if (state.running) {
+          return sendMessage(Msg.ALREADY_RUNNING)
+        }
+
+        start()
+
+        state.philosophers.forEach(p =>
+          p.on('message', payload => {
+            sendMessage(Msg.UPDATED, payload)
+          })
+        )
+        sendMessage(Msg.STARTED)
+      }
+
+      if (message === 'stop') {
+        if (!state.running) {
+          return sendMessage(Msg.ALREADY_STOPPED)
+        }
+        stop()
+        sendMessage(Msg.STOPPED)
+      }
+    })
   })
 })
 
 try {
   await fastify.listen({ port: 3000 })
+  console.log('Listening on port 3000')
 } catch (err) {
   if (err) {
     fastify.log.error(err)
